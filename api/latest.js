532debdbd0bd8e1599ac830a30dce4d901138c6a
Justin Portugal <>
2026-03-22 09:38:35 -0700

    Update: Sun Mar 22 09:38:35 PDT 2026

--- api/latest.js
+++ api/latest.js
@@ -1,1 +1,1 @@
-{"version":"20260322093800","url":"https://obol-mapp-github-io.vercel.app/bundle.zip","checksum":""}
+{"version":"20260322093834","url":"https://obol-mapp-github-io.vercel.app/bundle.zip","checksum":""}
