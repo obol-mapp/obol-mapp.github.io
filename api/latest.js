c16ea569a81c98717fcb33fea5256140f1687c70
Justin Portugal <>
2026-03-22 11:25:24 -0700

    Update: Sun Mar 22 11:25:24 PDT 2026

--- api/latest.js
+++ api/latest.js
@@ -1,1 +1,10 @@
-{"version":"20260322111259","url":"https://obol-mapp-github-io.vercel.app/bundle.zip","checksum":""}
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  res.status(200).json({
+    version: "20260322112524",
+    url: "https://obol-mapp-github-io.vercel.app/bundle.zip",
+    checksum: ""
+  });
+}
