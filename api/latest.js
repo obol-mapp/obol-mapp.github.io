7a152ec1ea429406a1e29bef37ddc5a19b17af48
Justin Portugal <>
2026-03-22 21:16:15 -0700

    Update: Sun Mar 22 21:16:15 PDT 2026

--- api/latest.js
+++ api/latest.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
   res.status(200).json({
-    version: "20260322211429",
+    version: "20260322211615",
     url: "https://obol-mapp-github-io.vercel.app/bundle.zip",
     checksum: ""
   });
